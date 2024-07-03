fa77806173cd0222e918d2788bd76e3bd262e7a3
wfbasconesdxc <wfbasconesdxc>
2024-07-03 08:46:10 +0000

    added files

---
+++ Example/(Clone) DE 1.2 create a notebook.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+print("I'm running Python" )
+
+# COMMAND ----------
+
+# MAGIC %run ./Includes/Classroom-Setup-01.2
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM demo_tmp_vw
+
+# COMMAND ----------
+
+
